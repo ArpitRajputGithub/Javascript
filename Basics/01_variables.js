a245b2bbc3e9a620e975ad3855749146cdc65dbb
ArpitRajputGithub <work.arpitrajput@gmail.com>
2024-04-19 21:21:52 +0530

    learned about variables and datatypes

---
+++ Basics/01_variables.js
@@ -0,0 +1,12 @@
+/* 
+Variables are containers for data. Every variable has a name and a value. We can get the value of the variable by using it's name.
+
+
+
+
+
+
+
+
+
+*/
---
+++ Basics/02_datatypes.js
@@ -0,0 +1,12 @@
+// Datatypes are diffrent types of variables. 
+// They can be like - Number, Boolean, Undefined, Null, String,
+
+let myNumber = 784;
+let myString = 'Hello this is arpit rajput';
+let myUndefined;
+let myNull = null;
+
+console.table([myNull, myNumber, myString,]);
+
+
+
