9f745719a714e20ad40936813b8a83b3d37750e5
ArpitRajputGithub <work.arpitrajput@gmail.com>
2024-04-21 22:02:19 +0530

    type conversion

---
+++ Basics/03_conversionOperations.js
@@ -0,0 +1,31 @@
+let number = 45;
+//console.log(number);
+//console.log(typeof number);
+//console.log(typeof(number));
+
+//convert 45 in string 
+let valueInstring = String(number);
+//console.log(valueInstring);
+//console.log(typeof valueInstring);
+
+//NaN - if you try to convert any string into a number, it wont
+ let x = "pizza";
+
+ x= Number(x);
+
+// console.log(x, typeof x); 
+
+
+// *********************************OPERATIONS*************************************
+
+//there is something like confusion in strings
+
+
+//console.log(3+3);                  //33 as Number
+//console.log("3"+3);                //33 as String
+//let abc = 3+"3"                    // 33 as String
+//console.log(abc, typeof abc);
+//console.log("3"+3+3);              //333 as string 
+//console.log(3+3+"3");              //63 as string
+
+
--- test.js
+++
@@ -1,1 +0,0 @@
-console.log("arpit rajput");
