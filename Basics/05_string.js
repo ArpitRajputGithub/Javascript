561b652c14948cfecca87f74ed34bdb5b34865da
ArpitRajputGithub <work.arpitrajput@gmail.com>
2024-04-27 22:54:22 +0530

    about strings

---
+++ Basics/05_string.js
@@ -0,0 +1,20 @@
+ let value = 111;
+ console.log(`Hello Arpit, Your value is ${value}`);
+ //string declaration --> 2 types
+
+ let myName = "Arpit Rajput";
+//  
+// console.log(myName);
+//  console.log(myName.length);
+//  console.log(myName.slice(1,7));
+//  console.log(myName.replace("Ra","Ta"));
+const myBranch = new String('CSE')
+console.log(myBranch);
+console.log(myBranch.length);
+//console.log(myBranch.toUpperCase);
+
+// const string4 = new String("Hello World");
+// console.log(string4.__proto__);
+// console.log(string4.toUpperCase);
+
+
