658d03bb70cfe1606d7dfde3cde4f849160723fa
ArpitRajputGithub <work.arpitrajput@gmail.com>
2024-05-09 19:40:52 +0530

    learned about numbers and maths in javascript

---
+++ Basics/06_NumsAndMaths.js
@@ -0,0 +1,20 @@
+const myNumber = 100;
+console.log(myNumber);
+//
+//const AnotherNumber = new Number (10.56665);
+//console.log(AnotherNumber);
+//
+////Methods -
+//
+////number to string 
+//console.log(AnotherNumber.toString());
+
+//fixed value (float)
+//console.log(AnotherNumber.toFixed(2));
+
+//precised value
+//console.log(AnotherNumber.toPrecision(2));
+
+
+
+//---------------------------------Maths------------------------------------------
